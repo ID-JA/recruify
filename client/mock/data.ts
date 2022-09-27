c54a168ef0e751675592bc563a52736de1ccddd2
ID-JA <idaissa93@gmail.com>
2022-09-27 21:37:14 +0100

    fix: remove generated data from faker

--- client/mock/data.ts
+++ client/mock/data.ts
@@ -1,4 +1,4 @@
-import { faker } from '@faker-js/faker'
+import { v4 as uuidv4 } from 'uuid'
 
 export interface ProfessionalSkillOption {
   readonly value: string
@@ -3828,24 +3828,190 @@ export type Job = {
   visitors: number
 }
 
-const newJob = (): Job => {
-  return {
-    id: faker.datatype.uuid(),
-    title: faker.name.jobTitle(),
-    company: faker.company.name(),
-    location: faker.address.cityName(),
-    createdAt: faker.date.past(2).toLocaleDateString(),
-    createdBy: faker.name.fullName(),
-    candidates: faker.datatype.number({
-      max: 20,
-      min: 0,
-    }),
-    visitors: faker.datatype.number({
-      max: 20,
-      min: 0,
-    }),
-    status: faker.helpers.arrayElement(['draft', 'active', 'unknown']),
-  }
-}
+// const newJob = (): Job => {
+//   return {
+//     id: faker.datatype.uuid(),
+//     title: faker.name.jobTitle(),
+//     company: faker.company.name(),
+//     location: faker.address.cityName(),
+//     createdAt: '20/02/2020',
+//     createdBy: faker.name.fullName(),
+//     candidates: faker.datatype.number({
+//       max: 20,
+//       min: 0,
+//     }),
+//     visitors: faker.datatype.number({
+//       max: 20,
+//       min: 0,
+//     }),
+//     status: faker.helpers.arrayElement(['draft', 'active', 'unknown']),
+//   }
+// }
 
-export const demoData: Job[] = new Array(20).fill({}).map(newJob)
+export const demoData: Job[] = [
+  {
+    id: uuidv4(),
+    title: 'JavaScript developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'draft',
+    candidates: 5,
+    visitors: 20,
+  },
+  {
+    id: uuidv4(),
+    title: 'JavaScript developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'draft',
+    candidates: 5,
+    visitors: 20,
+  },
+  {
+    id: uuidv4(),
+    title: 'Data Analytic Engineer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'active',
+    candidates: 5,
+    visitors: 12,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'Back end developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+  {
+    id: uuidv4(),
+    title: 'DotNet developer',
+    location: 'Aurora, CO, USA',
+    company: 'Google Labs',
+    createdAt: '09/01/23',
+    createdBy: 'Kent Smith',
+    status: 'blabla', // should render as badge
+    candidates: 0,
+    visitors: 2,
+  },
+]
