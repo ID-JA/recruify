26ee4310e642e53ab04340ce4b8bf0524c44d92b
Jamal Id Aissa <69154853+ID-JA@users.noreply.github.com>
2022-11-11 20:18:59 +0000

    delete unused files

--- client/components/Auth/sign-up/employer/steps/index.ts
+++
@@ -1,5 +0,0 @@
-import CompanyDetails from './CompanyDetails'
-import EmployerAccount from './EmployerAccount'
-import FinalizeDetails from './FinalizeDetails'
-
-export { CompanyDetails, FinalizeDetails, EmployerAccount }
