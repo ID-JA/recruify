aba02be14c67f5b0855e4078cee38aefc7da93bf
ID-JA <idaissa93@gmail.com>
2022-11-13 20:07:04 +0000

    update

--- client/pages/my-jobs/[id]/index.tsx
+++ client/pages/my-jobs/[id]/index.tsx
@@ -1,17 +1,8 @@
-import JobPreview from '@/components/jobs/job-preview'
 import { RootLayout } from '@/layouts/RootLayout/RootLayout'
 import { NextPageWithLayout } from '@/types'
-import { useRouter } from 'next/router'
 
 const JobPage: NextPageWithLayout = () => {
-  const router = useRouter()
-  const { id } = router.query as { id: string }
-
-  return (
-    <div>
-      <JobPreview id={id} />
-    </div>
-  )
+  return <div></div>
 }
 
 JobPage.getLayout = (page) => <RootLayout>{page}</RootLayout>
