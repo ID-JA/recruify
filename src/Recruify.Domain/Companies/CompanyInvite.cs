3dc522dec7ddd80e8808f797038ef3f9aeffcd2c
ID-JA <idaissa93@gmail.com>
2024-09-29 13:58:51 +0100

    create `Company`  domain modal - add company location entity - add company invite enity - add some domain event for the company

---
+++ src/Recruify.Domain/Companies/CompanyInvite.cs
@@ -0,0 +1,23 @@
+﻿using Recruify.Domain.Common;
+
+namespace Recruify.Domain.Companies;
+
+public class CompanyInvite : EntityBase<Guid>
+{
+
+    public string Email { get; private set; }
+    public Guid CompanyId { get; private set; }
+    public Guid Token { get; private set; }
+
+    public DateTime ExpireAt { get; private set; }
+    public DateTime CreatedAt { get; private set; }
+
+    public Company Company { get; } = null!;
+
+    public CompanyInvite(string email, Guid companyId, Guid token)
+    {
+        Email = email;
+        CompanyId = companyId;
+        Token = token;
+    }
+}
