d4e27f0f45b9fb63dd46f5802a05adea3f8de76f
ID-JA <idaissa93@gmail.com>
2024-11-09 14:04:31 +0100

    update application sidebar, remove unused code

--- src/Recruify.WebApp/app/(portal)/dashboard/page.tsx
+++ src/Recruify.WebApp/app/(portal)/dashboard/page.tsx
@@ -1,9 +1,3 @@
-import { ContentLayout } from "@/components/layout/content-layout"
-
 export default function DashboardPage() {
-  return (
-    <ContentLayout title="Dashboard">
-      <h1>This is Dashboard</h1>
-    </ContentLayout>
-  )
+  return <div>This is Dashboard</div>
 }
--- src/Recruify.WebApp/components/layout/user-dropdown.tsx
+++ src/Recruify.WebApp/components/layout/user-dropdown.tsx
@@ -1,13 +1,7 @@
 "use client"
 
 import Link from "next/link"
-import {
-  ChevronsUpDown,
-  LayoutGrid,
-  LogOut,
-  Settings,
-  User,
-} from "lucide-react"
+import { BadgeCheck, ChevronsUpDown, LogOut } from "lucide-react"
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {
@@ -22,13 +16,7 @@ import {
 import { Skeleton } from "@/components/ui/skeleton"
 
 import { signOut, useSession } from "../SessionProvider"
-import { Button } from "../ui/button"
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipProvider,
-  TooltipTrigger,
-} from "../ui/tooltip"
+import { SidebarMenuButton } from "../ui/sidebar"
 
 export function UserDropdown() {
   const { data: user } = useSession()
@@ -37,73 +25,72 @@ export function UserDropdown() {
     <>
       {user ? (
         <DropdownMenu>
-          <TooltipProvider disableHoverableContent>
-            <Tooltip delayDuration={100}>
-              <TooltipTrigger asChild>
-                <DropdownMenuTrigger asChild>
-                  <Button
-                    variant="outline"
-                    className="relative h-8 w-8 rounded-full"
-                  >
-                    <Avatar className="h-8 w-8">
-                      <AvatarImage
-                        src={
-                          user.imageUrl ||
-                          `https://api.dicebear.com/9.x/initials/svg?seed=${user.firstName}+${user.lastName}`
-                        }
-                        alt="Avatar"
-                      />
-                      <AvatarFallback className="bg-transparent">
-                        {user.firstName?.charAt(0)}
-                        {user.lastName?.charAt(0)}
-                      </AvatarFallback>
-                    </Avatar>
-                  </Button>
-                </DropdownMenuTrigger>
-              </TooltipTrigger>
-              <TooltipContent side="bottom">Profile</TooltipContent>
-            </Tooltip>
-          </TooltipProvider>
-
-          <DropdownMenuContent className="w-56" align="end" forceMount>
-            <DropdownMenuLabel className="font-normal">
-              <div className="flex flex-col space-y-1">
-                <p className="text-sm font-medium leading-none">
+          <DropdownMenuTrigger asChild>
+            <SidebarMenuButton
+              size="lg"
+              className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+            >
+              <Avatar className="h-8 w-8 rounded-lg">
+                <AvatarImage
+                  src={
+                    user.imageUrl ||
+                    `https://api.dicebear.com/9.x/initials/svg?seed=${user.firstName}+${user.lastName}`
+                  }
+                  alt={user.firstName}
+                />
+                <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+              </Avatar>
+              <div className="grid flex-1 text-left text-sm leading-tight">
+                <span className="truncate font-semibold">
                   {user.firstName} {user.lastName}
-                </p>
-                <p className="text-xs leading-none text-muted-foreground">
-                  {user.email}
-                </p>
+                </span>
+                <span className="truncate text-xs">{user.email}</span>
+              </div>
+              <ChevronsUpDown className="ml-auto size-4" />
+            </SidebarMenuButton>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent
+            className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+            side="bottom"
+            align="end"
+            sideOffset={4}
+          >
+            <DropdownMenuLabel className="p-0 font-normal">
+              <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+                <Avatar className="h-8 w-8 rounded-lg">
+                  <AvatarImage
+                    src={
+                      user.imageUrl ||
+                      `https://api.dicebear.com/9.x/initials/svg?seed=${user.firstName}+${user.lastName}`
+                    }
+                    alt={user.firstName}
+                  />
+                  <AvatarFallback className="rounded-lg">CN</AvatarFallback>
+                </Avatar>
+                <div className="grid flex-1 text-left text-sm leading-tight">
+                  <span className="truncate font-semibold">
+                    {user.firstName} {user.lastName}
+                  </span>
+                  <span className="truncate text-xs">{user.email}</span>
+                </div>
               </div>
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
-              <DropdownMenuItem className="hover:cursor-pointer" asChild>
-                <Link href="/dashboard" className="flex items-center">
-                  <LayoutGrid className="mr-3 h-4 w-4 text-muted-foreground" />
-                  Dashboard
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="hover:cursor-pointer" asChild>
-                <Link href="/account" className="flex items-center">
-                  <User className="mr-3 h-4 w-4 text-muted-foreground" />
+              <DropdownMenuItem asChild>
+                <Link href="/settings/profile" className="flex items-center">
+                  <BadgeCheck />
                   Account
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem className="hover:cursor-pointer" asChild>
-                <Link href="/settings" className="flex items-center">
-                  <Settings className="mr-3 h-4 w-4 text-muted-foreground" />
-                  Settings
                 </Link>
               </DropdownMenuItem>
             </DropdownMenuGroup>
             <DropdownMenuSeparator />
             <DropdownMenuItem
-              className="hover:cursor-pointer"
               onClick={() => signOut({ callbackUrl: "/sign-in" })}
             >
-              <LogOut className="mr-3 h-4 w-4 text-muted-foreground" />
-              Sign out
+              <LogOut />
+              Log out
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
